31f329cb032ce783e515a0cc7626edce014eb7a3
Eshan-shaik07 <eshanahmedgithub@gmail.com>
2024-11-20 16:44:52 +0530

    i have created a new python file

---
+++ eshan.py
@@ -0,0 +1,10 @@
+name = "eshan ahmed"
+CollageName = "schoolofmines"
+print(name)
+print(CollageName)
+is_adult = int(input(print("what is ur age")))
+if is_adult  <18:
+    print("false")
+elif is_adult  >18:
+    print("true")
+print(is_adult)
